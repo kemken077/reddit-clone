21f5a6ef69101e7cae7f5c9586dd7bafe1ce9671
¨kemken077¨ <kemken077@gmail.com>
2023-02-03 11:43:07 -0300

    Add Chakra theme file

---
+++ app/src/chakra/theme.ts
@@ -0,0 +1,13 @@
+import "@fontsource/open-sans"
+import { extendTheme } from "@chakra-ui/react"
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      100: '#FF3c00',
+    },
+  },
+  fonts: {
+    body: 'Opens Sans, sans-serif',
+  },
+})
