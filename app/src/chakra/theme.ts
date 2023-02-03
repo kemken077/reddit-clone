bf81ea26b25dc624e6e71e33d83493273a5df129
¨kemken077¨ <kemken077@gmail.com>
2023-02-03 11:52:59 -0300

    Some theme config

--- app/src/chakra/theme.ts
+++ app/src/chakra/theme.ts
@@ -1,4 +1,6 @@
-import "@fontsource/open-sans"
+import "@fontsource/open-sans/300.css"
+import "@fontsource/open-sans/400.css"
+import "@fontsource/open-sans/700.css"
 import { extendTheme } from "@chakra-ui/react"
 
 export const theme = extendTheme({
@@ -9,5 +11,15 @@ export const theme = extendTheme({
   },
   fonts: {
     body: 'Opens Sans, sans-serif',
+  },
+  styles: {
+    global: () => ({
+      body: {
+        bg: 'gray.200',
+      }
+    })
+  },
+  components: {
+    // Button
   },
 })
