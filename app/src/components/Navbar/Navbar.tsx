1acb15ff2f6b224a2569f59535cf50535ef37757
¨kemken077¨ <kemken077@gmail.com>
2023-02-07 19:49:51 -0300

    SearchInput component

--- app/src/components/Navbar/Navbar.tsx
+++ app/src/components/Navbar/Navbar.tsx
@@ -1,10 +1,16 @@
+import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
-
-type NavbarProps = {
-  
-}
+import SearchInput from './SearchInput'
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="page logo" height="30px" />
+        <Image src="/images/redditText.svg" alt="" height="46px" display={{ base: 'none', md: 'unset' }} />
+      </Flex>
+      <SearchInput />
+    </Flex>
+  )
 }
 export default Navbar
